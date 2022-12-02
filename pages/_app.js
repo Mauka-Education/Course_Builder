b9fb954d17787675e9c4867aea097a7999718fc1
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-12-02 16:05:13 +0530

    added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/css/index.css";
+import 'react-quill/dist/quill.snow.css';
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import 'react-quill/dist/quill.snow.css'
 
 import { store, persistor } from "../redux";
 
